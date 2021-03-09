000b92b0e920d169c4216fb64453eaf41fea967c
Anzor Shogenov <anzor9657@gmail.com>
2021-03-09 08:26:38 +0300

    added desktop

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,285 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        urusov-stroy
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="main">
+    <div class="padding-100">
+      <b-img
+        src="../static/images/big_image.png"
+        fluid
+        alt="Responsive image"
+      />
+      <div class="top-title-container">
+        <span class="top-title">urusovstroy</span>
+        <span class="font-weight-500">от идеи до реализации</span>
+      </div>
+      <div class="feedback">
+        <div class="d-flex flex-column">
+          <span class="title">Связаться с нами</span>
+          <span class="action">Заказать звонок сейчас</span>
+        </div>
       </div>
     </div>
+    <div class="row padding-150 text-center align-items-center">
+      <div class="col font-weight-bold">01</div>
+      <div class="col font-weight-bold">Кто мы</div>
+      <div class="col font-weight-bold">О компании</div>
+    </div>
+    <div class="row justify-content-center padding-100">
+      <div class="col-md-3 normal-text">
+        Команда профессионалов
+      </div>
+      <div class="col-md-6 big-text">
+        Амбициозная и уверенная в себе команда с четырехлетним успешным опытом
+        реализации проектов разного уровня сложности
+        <div class="row small-text mt-4">
+          <div class="col-md">
+            Мы удовлетворяем эстетические потребности наших клиентов, предлагая
+            уникальную методику работы и высококлассный сервис. Мы берем на себя
+            все заботы начиная с замысла до самого новоселья
+          </div>
+          <div class="col-md">
+            Больше всего в нашей работе мы заботимся о функциональности
+            интерьера, стремясь создать максимально удобные условия для жизни.
+            Наш успех предопределен ответственным подходом к работе всей нашей
+            команды
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row padding-150 justify-content-center align-items-center">
+      <div class="col-md-5 font-weight-bold">
+        <b-img src="../static/images/person.png" fluid alt="person image" />
+      </div>
+      <div class="col-md-5 font-weight-bold big-padding">
+        <div class="padding-50">
+          <div class="big-text">
+            Урусов Алим Отчество
+          </div>
+          <div class="gray-big-text">
+            Основатель
+          </div>
+        </div>
+        <div class="small-text padding-50">
+          Каждый заказчик индивидуален и помимо стилевых предпочтений имеет
+          особенности образа жизни. Поэтому, больше всего в нашей работе мы
+          заостряем внимание на функциональности интерьера, стремясь создать
+          максимально удобные и полезные условия для жизни.
+        </div>
+        <div class="small-text padding-50">
+          Наш успех определяет наша эксклюзивность, качество услуг и
+          ответственный подход к работе всей нашей команды единомышленников,
+          разделяющих наши ценности.
+        </div>
+        <div class="gray-small-text padding-50">
+          Хотите познакомиться со всеми?
+        </div>
+        <div class="normal-text underline">
+          Сотрудники компании
+        </div>
+      </div>
+    </div>
+    <div class="row padding-150 text-center align-items-center">
+      <div class="col font-weight-bold">02</div>
+      <div class="col font-weight-bold">Что мы предлагаем</div>
+      <div class="col font-weight-bold">Наши услуги</div>
+    </div>
+    <div class="row justify-content-end padding-150">
+      <div class="col-5 offset-2 mr-auto">
+        <div class="row padding-100">
+          <div class="col-5 mr-auto">
+            <div class="font-weight-bold mb-3">
+              01
+            </div>
+            <div class="normal-text mb-2">
+              Дизайн интерьера
+            </div>
+            <div class="small-text padding-50">
+              Выполняем комплексное проектирование коммерческих и жилых объектов
+              с полным пакетом документов
+            </div>
+          </div>
+          <div class="col-5 mr-auto">
+            <div class="font-weight-bold mb-3">
+              02
+            </div>
+            <div class="normal-text mb-2">
+              Проектирование
+            </div>
+            <div class="small-text padding-50">
+              Выполняем комплексное проектирование коммерческих и жилых объектов
+              с полным пакетом документов
+            </div>
+          </div>
+          <div class="col-5 mr-auto">
+            <div class="font-weight-bold mb-3">
+              03
+            </div>
+            <div class="normal-text mb-2">
+              Строительство
+            </div>
+            <div class="small-text padding-50">
+              Начиная от первого кирпича, заканчивая новосельем, мы построим дом
+              европейского уровня
+            </div>
+          </div>
+          <div class="col-5 mr-auto">
+            <div class="font-weight-bold mb-3">
+              04
+            </div>
+            <div class="normal-text mb-2">
+              Ремонт
+            </div>
+            <div class="small-text padding-50">
+              Электрики, сантехники, плиточники, моляры. Лучшие специалисты в
+              одной компании
+            </div>
+          </div>
+          <div class="col-5 mr-auto">
+            <div class="font-weight-bold mb-3">
+              05
+            </div>
+            <div class="normal-text mb-2">
+              Корпусная мебель
+            </div>
+            <div class="small-text padding-50">
+              Мы используем в наших проектах мебель, выполненную по
+              индивидуальным эскизам
+            </div>
+          </div>
+          <div class="col-5 mr-auto">
+            <div class="font-weight-bold mb-3">
+              06
+            </div>
+            <div class="normal-text mb-2">
+              Интерьерный салон
+            </div>
+            <div class="small-text padding-50">
+              Эксклюзивные материалы дают нам возможность делать уникальный
+              интерьер
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5">
+        <b-img src="../static/images/image_1.png" fluid alt="image_1" />
+      </div>
+    </div>
+    <!-- <div>
+      <h1>Hello Nuxters! 👋</h1>
+      <p>
+        This page is rendered on the <strong>{{ rendering }}</strong>
+      </p>
+      <p v-if="rendering === 'server'">
+        First load or hard refresh is done on server side.
+      </p>
+      <p v-if="rendering === 'client'">Navigation is done on client side.</p>
+      <ul>
+        <li>Refresh the page for server side rendering.</li>
+        <li>Click the links to see client side rendering.</li>
+      </ul>
+      <NuxtLink to="/about">About Page</NuxtLink>
+    </div> -->
   </div>
 </template>
-
 <script>
-export default {}
+export default {
+  head: {
+    title: "Home page",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "Home page description"
+      }
+    ]
+  },
+  asyncData() {
+    return {
+      rendering: process.server ? "server" : "client"
+    };
+  }
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style scoped>
+.main {
+  width: 100%;
+  overflow: hidden;
+}
+.top-title-container {
+  position: absolute;
+  top: 20vw;
+  left: 10.2vw;
+  text-transform: uppercase;
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+  font-size: 2.25rem;
+}
+.top-title {
+  font-size: 4.75rem;
+}
+.feedback {
+  position: absolute;
+  top: 41.8vw;
+  right: 0;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  width: 33vw;
+  height: 14vw;
+  background-color: #fff;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.feedback div .title {
+  font-size: 1.4375rem;
+  font-weight: 500;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.feedback div .action {
+  font-size: 1.25rem;
+  font-weight: 500;
+  color: #adadad;
 }
-
-.links {
-  padding-top: 15px;
+.big-text {
+  font-weight: 600;
+  font-size: 2.25rem;
+  line-height: 160%;
+}
+.normal-text {
+  font-weight: 600;
+  font-size: 1.75rem;
+  line-height: 2.875rem;
+}
+.small-text {
+  font-weight: 500;
+  font-size: 1.125rem;
+  line-height: 200%;
+}
+.gray-big-text {
+  font-weight: 600;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  color: #adadad;
+}
+.gray-small-text {
+  font-weight: 600;
+  font-size: 0.8125rem;
+  line-height: 1.25rem;
+  letter-spacing: 0.02rem;
+  text-transform: uppercase;
+  color: #6d6d6d;
+}
+.big-padding {
+  padding: 150px;
+}
+.padding-50 {
+  padding-bottom: 50px;
+}
+.padding-100 {
+  padding-bottom: 100px;
+}
+.padding-150 {
+  padding-bottom: 150px;
+}
+.underline {
+  width: fit-content;
+  border-bottom: 2px solid;
+  padding-bottom: 5px;
 }
 </style>
---
+++ pages/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-container fluid="sm"> </b-container>
+    <b-container fluid="md">
+      <b-navbar toggleable type="light" variant="transparent">
+        <b-navbar-brand href="#"
+          ><NuxtLink to="/"><Logo /></NuxtLink
+        ></b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="d-flex justify-content-around w-100">
+            <b-nav-item href="#">ДИЗАЙН ИНТЕРЬЕРА</b-nav-item>
+            <b-nav-item href="#">АРХИТЕКТУРНОЕ ПРОЕКТИРОВАНИЕ</b-nav-item>
+            <b-nav-item href="#">СТРОИТЕЛЬСТВО</b-nav-item>
+            <b-nav-item href="#">РЕМОНТ</b-nav-item>
+            <b-nav-item href="#">КОРПУСНАЯ МЕБЕЛЬ</b-nav-item>
+            <b-nav-item href="#">ИНТЕРЬЕРНЫЙ САЛОН</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </b-container>
+    <b-container fluid="lg"> </b-container>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+<style scoped>
+.nav-item {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+}
+</style>

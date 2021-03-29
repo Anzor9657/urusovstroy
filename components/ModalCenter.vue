c2a9c1999b90c6b4182219fba67ec236f8ab5355
Anzor Shogenov <crack.anzor@gmail.com>
2021-03-29 16:14:37 +0300

    added modal images

--- components/ModalCenter.vue
+++ components/ModalCenter.vue
@@ -1,15 +1,48 @@
 <template> 
-    <b-modal id="modal-center" centered title="BootstrapVue">
-      <p class="my-4">{{content && content.title}}</p>
+    <b-modal id="modal-center" centered hide-footer hide-header>
+        <div class="pa-5">
+            <vue-slick-carousel :arrows="false" v-bind="carouselSettings" :dots="true">
+                <template v-for="item of images">
+                    <div :key="item" class="img-block">
+                        <b-img src="../static/images/default.png" alt="card image" />
+                    </div>
+                </template>
+            </vue-slick-carousel>
+        </div>
     </b-modal>
 </template>
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  // optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
+    components: { VueSlickCarousel },
     props: {
         content: {
             type: Object,
             default: () => {}
         }
+    },
+    data() {
+        return {
+            carouselSettings: {
+                centerMode: true,
+                infinite: false,
+                slidesToShow: 1,
+                swipeToSlide: true,
+                touchMove: true,
+                swipe: true
+            },
+            images: [1,2,3,4,5,6]
+        }
     }
 }
 </script>
+<style lang="scss" scoped>
+.img-block {
+    max-width: 99%;
+    height: 70vh;
+    overflow: hidden;
+}
+</style>
